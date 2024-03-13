cbcb8077933f71d6c2eb80e9fda4d4d3523da616
Manuel26190 <manuelfaure26190@gmail.com>
2024-03-13 10:40:26 +0100

    save

---
+++ index.js
@@ -0,0 +1,6 @@
+// Gestion du scrollTop
+function scrollToTop() {
+    document.body.scrollTop = 0 // Pour les navigateurs Chrome, Firefox, IE et Opera
+    document.documentElement.scrollTop = 0 // Pour Safari
+    console.log('test click ')
+}
